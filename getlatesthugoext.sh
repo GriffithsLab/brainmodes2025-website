8647321e6b105a44a7968402eae8ff76994855d5
John Griffiths <JohnGriffiths@users.noreply.github.com>
2025-06-05 19:03:59 +0000

    added a shell script to pull latest hugo extended

---
+++ getlatesthugoext.sh
@@ -0,0 +1,7 @@
+HUGO_VERSION="0.125.4"
+HUGO_TAR="hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
+wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TAR}
+tar -xzf ${HUGO_TAR} hugo
+chmod +x hugo
+rm $HUGO_TAR
+# sudo mv hugo /usr/local/bin/hugo
