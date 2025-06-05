a231d2d31143ede57ac5a924501aee68c104d426
John Griffiths <JohnGriffiths@users.noreply.github.com>
2025-06-05 19:07:34 +0000

    changed name of it

--- getlatesthugoext.sh
+++ getlatesthugoext.sh
@@ -3,5 +3,6 @@ HUGO_TAR="hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
 wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TAR}
 tar -xzf ${HUGO_TAR} hugo
 chmod +x hugo
+mv hugo hugo_ext
 rm $HUGO_TAR
 # sudo mv hugo /usr/local/bin/hugo
